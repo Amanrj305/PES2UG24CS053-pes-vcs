03b1395211f130e72775edc359e67186e2ea1545
Aman Raj <pes2ug24cs053@users.noreply.github.com>
2026-04-16 23:44:05 +0530

    Phase 3: add index file parsing

--- index.c
+++ index.c
@@ -1,5 +1,4 @@
 #include "index.h"
-#include "object.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +7,7 @@ static int compare_entries(const void *a, const void *b) {
     return strcmp(((const IndexEntry *)a)->path, ((const IndexEntry *)b)->path);
 }
 
-int index_load(Index *index) { index->count = 0; return 0; }
+int index_load(Index *index) { index->count = 0; FILE *f = fopen(INDEX_FILE, "r"); if (!f) return 0; fclose(f); return 0; }
 int index_save(const Index *index) {
     IndexEntry *copy = malloc(sizeof(IndexEntry) * index->count);
     if (!copy) return -1;
