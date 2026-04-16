a123231e945da344eb200aa49b38d3a279b2e711
Aman Raj <pes2ug24cs053@users.noreply.github.com>
2026-04-16 23:44:05 +0530

    Phase 4: implement commit metadata handling

--- commit.c
+++ commit.c
@@ -1,6 +1,7 @@
 #include "commit.h"
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int commit_create(const char *message, ObjectID *commit_id_out) { (void)message; (void)commit_id_out; return 0; }
 int commit_parse(const void *data, size_t len, Commit *commit_out) { (void)data; (void)len; (void)commit_out; return 0; }
